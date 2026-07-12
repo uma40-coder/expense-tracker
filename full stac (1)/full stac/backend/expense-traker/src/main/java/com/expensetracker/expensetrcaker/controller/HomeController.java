950ae1156182790743df6e605d8a3d975f6ca054
Uma <naniuma40@gmail.com>
2026-07-12 16:23:56 +0530

    star

--- full stac (1)/full stac/backend/expense-traker/src/main/java/com/expensetracker/expensetrcaker/controller/HomeController.java
+++ full stac (1)/full stac/backend/expense-traker/src/main/java/com/expensetracker/expensetrcaker/controller/HomeController.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/expenses")
 public class HomeController {
