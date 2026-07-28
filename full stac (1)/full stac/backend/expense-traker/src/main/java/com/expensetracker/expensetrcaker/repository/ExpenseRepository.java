439f5586dede6da10926c38fbf72cdb6a38882d2
uma <naniuma40@gmail.com>
2026-07-28 09:49:59 +0530

    h

--- full stac (1)/full stac/backend/expense-traker/src/main/java/com/expensetracker/expensetrcaker/repository/ExpenseRepository.java
+++
@@ -1,7 +0,0 @@
-package  com.expensetracker.expensetrcaker.repository;
-import com.expensetracker.expensetrcaker.entity.Expense;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExpenseRepository extends JpaRepository<Expense, Integer> {
-
-}
--- full stac (1)/full stac/frontend/src/component/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer>
-      <p>&copy; 2023 Expense Tracker. All rights reserved.</p>
-    </footer>
-  );
-}
-
-export default Footer;
